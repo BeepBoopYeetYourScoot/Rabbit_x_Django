6dd6b793eab0832335af54369a7b60af59f5feff
BeepBoopYeetYourScoot <dovakino98@gmail.com>
2021-10-15 13:40:19 +0500

    Tutorial 2: Стандартная рабочая очередь (Work Queue)

--- rabbit/producer.py
+++ rabbit/producer.py
@@ -1,7 +1,9 @@
-from pika import BlockingConnection, ConnectionParameters
+import sys
+
+from pika import BlockingConnection, ConnectionParameters, BasicProperties
 
 
-def queue_send_hello():
+def producer_send_message(message: str):
     # Присоединяемся к брокеру на локалхосте, открыв с ним соединение
     connection = BlockingConnection(ConnectionParameters('localhost'))
 
@@ -9,17 +11,22 @@ def queue_send_hello():
     channel = connection.channel()
 
     # Объявляем очередь, в которую будут отправляться сообщения
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='task_queue')
 
     # Отправляем сообщение в очередь
     # Note 1: Все сообщения должны проходить через обменники (exchanges)
     # Note 2: В данном примере отправка осуществляется через стандартный обменник,
     #         поэтому необходимо указать имя очереди (routing_key),
     #         в которую должно быть отправлено сообщение
-    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    channel.basic_publish(exchange='',
+                          routing_key='task_queue',
+                          body=message,
+                          # delivery_mode=2 sets message as durable
+                          properties=BasicProperties(delivery_mode=2))
 
     print('[x] Message sent!')
 
     # Закрываем соединение, чтобы удостовериться, что сетевые буферы были очищены
     # и что сообщение было доставлено в очередь
     connection.close()
+
--- rabbit/worker.py
+++ rabbit/worker.py
@@ -1,4 +1,4 @@
-import sys
+import sys, time
 
 from pika import BlockingConnection, ConnectionParameters
 
@@ -7,20 +7,25 @@ def main():
     connection = BlockingConnection(ConnectionParameters('localhost'))
     channel = connection.channel()
 
-    channel.queue_declare('hello')
+    channel.queue_declare('task_queue', durable=True)
 
     # Получение сообщений работает с помощью привязки callback-функции к очереди
     # При получении сообщения pika вызовет данную функцию
 
     def callback(ch, method, properties, body):
-        print(f'[x] Received {body}')
+        print(f'[x] Received {body.decode()}')
+        time.sleep(body.count(b'.'))
+        print('[V] Done!')
+        # Подтверждаем получение и обработку сообщения
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_qos(prefetch_count=1)
 
     # Говорим, что при получении сообщения в очереди hello нужно вызывать callback-функцию
     # По идее, для того, чтобы ловить сообщения из данной очереди, мы должны точно знать,
     # что данная очередь существует на нашей стороне.
     # Нам это известно, потому что мы объявили очередь выше
     channel.basic_consume(queue='hello',
-                          auto_ack=True,
                           on_message_callback=callback)
 
     print('[*] Waiting for messages. To exit: Press CTRL+C')
