17e05b0c97eb274f995dc890303cc4dfb850fc1a
BeepBoopYeetYourScoot <59568155+BeepBoopYeetYourScoot@users.noreply.github.com>
2021-10-18 23:16:38 +0300

    Научил DRF создавать несколько объектов за раз

--- rabbit/views.py
+++ rabbit/views.py
@@ -23,3 +23,12 @@ class ProductViewSet(ModelViewSet):
     @action(detail=False, url_path='')
     def upload_excel(self, request):
         pass
+
+    def create(self, request, *args, **kwargs):
+        is_many = isinstance(request.data, list)
+
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
